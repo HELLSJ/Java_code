fefbfdc77224663cf9baa50cea9a0c775fd00d88
李承轩 <1206817338@qq.com>
2025-12-15 20:29:25 +0800

    JavaSE数据结构复习

--- src/Sort/eightSortMethods.java
+++ src/Sort/eightSortMethods.java
@@ -15,4 +15,48 @@ public class eightSortMethods {
             array[j+1] = tmp;
         }
     }
+
+    //Ï£¶ûÅÅĞò
+    public static void shellSort(int[] array){
+        int gap = array.length;
+        while(gap>1) {
+            gap /= 2;
+            shell(array, gap);
+        }
+    }
+
+    private static void shell(int[] array, int gap) {
+        for (int i = gap; i < array.length; i++) {
+            int tmp = array[i];
+            int j = i-tmp;
+            for(;j>=0;j-=gap){
+                if (array[j]>tmp){
+                    array[j+gap]=array[i];
+                }else{
+                    break;
+                }
+            }
+            array[j+gap] = tmp;
+        }
+    }
+
+    //Ö±½ÓÑ¡ÔñÅÅĞò
+    public static void swap(int[] array, int i, int j){
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public static void selectSort(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+    }
+
 }
